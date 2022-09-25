869f363c75c629af2903025658a3fe10e3742279
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-09-25 12:23:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./components/Header";
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="container">
       <Header/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
