d4dfedd0115a9c62bb23e294034442f3ffe31de4
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-10-01 21:47:14 +0530

    01:25:00

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
-    const [tasks, setTasks]=useState([{id: 1, text: "Doctors Appointment", day: "Feb 5th @ 2:30pm", reminder: true}, {id: 2, text: "Meeting at School", day: "Feb 6th @ 1:30pm", reminder: true}, {id: 3, text: "Food Shopping", day: "Feb 5th @ 2:30pm", reminder: false}])
+
+    const [showAddTask, setShowAddTask]=useState(false)
+
+    const [tasks, setTasks]=useState([])
 
     const addTask=(task)=>{
         const id=Math.floor(Math.random()*10000)+1
@@ -22,11 +25,11 @@ const App = () => {
 
     return (
         <div className="container">
-            <Header/>
-            <AddTask onAdd={addTask}/>
+            <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+            {showAddTask && <AddTask onAdd={addTask}/>}
             {tasks.length>0?(<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):("No Tasks to show")}
         </div>
-    );
-};
+    )
+}
 
-export default App;
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
 import PropTypes from 'prop-types'
 import Button from "./Button";
 
-const Header = ({title}) => {
-    const onClick=()=>{
-        console.log('Clicked')
-    }
+const Header = ({title, onAdd, showAdd}) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button onClick={onClick} color="green" text="Add"/>
+            <Button onClick={onAdd} color={showAdd?'red':'green'} text={showAdd?'Close': 'Add'}/>
         </header>
     )
 }
+
+
 
 Header.defaultProps={
     title: "Task Tracker"
