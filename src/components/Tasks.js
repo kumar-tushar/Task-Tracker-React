375f5a8ba9d9c6190833aba9a8fac51432a0d95b
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-10-01 22:13:42 +0530

    01:30:00

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,7 @@ import Task from "./Task";
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <>
-            {tasks.map( (task) => ( <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>) )}
+            {tasks.map( (task, index) => ( <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle}/>) )}
         </>
     )
 }
